02efbf64335a1be03bb149fc48d7208c42efe3da
ydennekrf <ydennekrf@gmail.com>
2022-07-12 08:12:34 -0400

    added server and connection files

---
+++ Config/connection.js
@@ -0,0 +1,8 @@
+const { connect, connection } = require('mongoose');
+
+connect('mongodb://127.0.0.1:27017/social_DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection
